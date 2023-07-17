3e5bf671ff7400e9fa310de7e61fbc018b7993f1
Hawa Ali <hawad416@gmail.com>
2023-07-16 17:06:45 -0700

    ok

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello{
+    public static void main(String[] args) {
+            System.out.println("welcome to the best section ever");
+            System.out.println("it's going to be so lit");
+    }
+}
+
